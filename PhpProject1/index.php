4011e5e602ab5e4a3171f1fb985a4b2662dcb567
aluno <aluno@10.2.238.222>
2015-06-18 20:28:26 -0300

    Explicando

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -16,6 +16,7 @@ and open the template in the editor.
         echo "Fazendo update";
         echo "outro update";
         echo "updateeeeeeeeee";
+        echo "comitaaaaando";
         ?>
     </body>
 </html>
