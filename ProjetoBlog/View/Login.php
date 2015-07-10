525e4c0af4969dae107aa0e5e2762c05f0950538
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-09 22:22:52 -0300

    Melhorias no visual página login

--- ProjetoBlog/View/Login.php
+++ ProjetoBlog/View/Login.php
@@ -15,38 +15,41 @@ and open the template in the editor.
 
     </head>
     <body style="background: #eee;">
-
-        <div class="container">
-               <p><br></p>
-               <div class="row">
-                      <div class="col-md-8"></div>
-                      <div class="col-md-4">
-                             <div class="panel panel-default">
-                                    <div class="panel-body">
-                                              <div class="page-header">
-                                                   <h3>Login</h3>
-                                              </div>
-                                              <form>
-                                                        <div class="form-group">
-                                                                  <label for="exampleInputEmail1">Usuário</label>
-                                                                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
-                                                        </div>
-                                                        <div class="form-group">
-                                                                  <label for="exampleInputPassword1">Senha</label>
-                                                                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
-                                                        </div>
-                                                  <hr/>
-                                                        <button type="submit" class="btn btn-primary">Login</button>
-                                                        <p></br></p>
-                                              </form>
-                                    </div>
-                             </div>
-                      </div>
-               </div>
-        </div>   
-
-
-
-
+        <div class="principal">
+            <div class="container">
+                   <p><br></p>
+                   <div class="row">
+                          <div class="col-md-8"></div>
+                          <div class="col-md-4">
+                                 <div class="panel panel-default">
+                                        <div class="panel-body">
+                                                  <div class="page-header">
+                                                       <h3>Login</h3>
+                                                  </div>
+                                                  <form>
+                                                            <div class="form-group">
+                                                                      <label for="exampleInputEmail1">Usuário</label>
+                                                                      <div class="input-group">
+                                                                          <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+                                                                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
+                                                                      </div>
+                                                            </div>
+                                                            <div class="form-group">
+                                                                      <label for="exampleInputPassword1">Senha</label>
+                                                                    <div class="input-group">
+                                                                            <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-star"></span></span>
+                                                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+                                                                      </div>
+                                                            </div>
+                                                      <hr/>
+                                                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock"></span>Login</button>
+                                                            <p></br></p>
+                                                  </form>
+                                        </div>
+                                 </div>
+                          </div>
+                   </div>
+            </div>   
+        </div> 
     </body>
 </html>
