783fbe3658089ca2cb5a73cf6dee85dced8c9e02
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-10 16:56:49 -0300

    Home

---
+++ ProjetoBlog/View/home.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="../estilo/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+       <div class="navbar navbar-inverse navbar-static-top">
+           <div class="container">
+               
+               <a href="#" class="navbar-brand">Brogão</a>
+               
+               <button class="navbar-toggle" data-toggle ="collapse" data-target =".navHeaderCollapse"
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+               </button>
+               
+               <div class="collapse navbar-collapse navHeaderCollapse">
+                   
+                   <ul class="nav navbar-nav navbar-right">
+                       
+                       <li class="active"><a href="#">Home</a></li>
+                       <li class="dropdown">
+                           
+                           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Redes Sociais<b class="caret"></b></a>
+                           <ul class="dropdown-menu">
+                               <li><a href="#">Twitter</a></li>
+                               <li><a href="#">Facebook</a></li>
+                               <li><a href="#">Instagram</a></li>
+                           </ul>
+                       </li>
+                       <li><a href="#">Sobre o Blog</a></li>
+
+                   
+                   </ul>
+                   
+               </div>
+               
+           </div>
+            
+        </div>
+        
+        
+        <div class="container">
+            
+            <div class="jumbotron">
+                <center><h1>Bem Vindo!</h1>
+                    <p>Ao melhor blog de todos os tempos!</p>
+                    <a class="btn btn-default">Clique aqui!</a>
+                    <a class="btn btn-info">Compartilhe!</a></center>        
+            </div>
+            
+        </div>
+        
+       <div class="container">
+            
+            <div class="row">
+                    
+                <div class="col-md-3">
+                    
+                    <h3><a href="#">Artigo 1 do brog</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
+                    <a href="#" class="btn btn-default">Leia Mais</a>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                    <h3><a href="#">Artigo 2 do brog</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
+                    <a href="#" class="btn btn-default">Leia Mais</a>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                    <h3><a href="#">Artigo 3 do brog</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
+                    <a href="#" class="btn btn-default">Leia Mais</a>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                    <h3><a href="#">Artigo 4 do brog</a></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
+                    <a href="#" class="btn btn-default">Leia Mais</a>
+                    
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        
+        
+        <div class="navbar navbar-default navbar-fixed-bottom">
+            
+            <div class="container">
+                
+                <p class="navbar-text pull-left">Site desenvolvido por Equipe Blog</p>
+                <a href="https://github.com/IFGO/blog" class="navbar-btn btn-danger btn pull-right">Código no GitHub</a>
+                
+            </div>
+            
+        </div>
+        
+    
+        
+        
+        
+    </body>
+</html>
+
