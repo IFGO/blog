3cd11974b8791392fa0ff8b9e837f168fe5eb03e
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-15 09:48:41 -0300

    Página post

---
+++ ProjetoBlog/View/post.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="../estilo/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../estilo/css/styles2.css" rel="stylesheet">
+    </head>
+    <body>
+        
+       <div class="navbar navbar-inverse navbar-static-top">
+           <div class="container">
+               
+               <a href="#" class="navbar-brand">Brogão</a>
+               
+               <button class="navbar-toggle" data-toggle ="collapse" data-target =".navHeaderCollapse"
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+               </button>
+               
+               <div class="collapse navbar-collapse navHeaderCollapse">
+                   
+                   <ul class="nav navbar-nav navbar-right">
+                       
+                       <li class="active"><a href="#">Home</a></li>
+                       <li class="dropdown">
+                           
+                           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Redes Sociais<b class="caret"></b></a>
+                           <ul class="dropdown-menu">
+                               <li><a href="#">Twitter</a></li>
+                               <li><a href="#">Facebook</a></li>
+                               <li><a href="#">Instagram</a></li>
+                           </ul>
+                       </li>
+                       <li><a href="#">Sobre o Blog</a></li>
+
+                   </ul>
+                   
+               </div>
+               
+           </div>
+            
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+                    <div class="panel panel-default"> 
+                        <div class="panel-body">
+                            
+                            <div class="page-header">
+                                <h3>test teste testet tsetstete <small>Postado em 10 de junho</small></h3>
+                            </div>
+                            
+                            <img class="featureImg" src="imagens/android.jpg" width="100%">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam commodo id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut pulvinar sapien nec dolor viverra malesuada. Quisque ac justo a mauris vulputate cursus. Sed non mauris neque. Pellentesque tincidunt egestas enim at congue. Nulla congue, metus eu rhoncus vulputate, metus risus suscipit orci, sed congue augue justo sit amet metus.</p>
+                            
+                            <h4>Texto...</h4>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-lg-3">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item active">
+                            <h4 class="list-group-item-heading">Loren Ipsum</h4>
+                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam</p>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">Loren Ipsum</h4>
+                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam</p>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">Loren Ipsum</h4>
+                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam</p>
+                        </a>
+                        <a href="#" class="list-group-item">
+                            <h4 class="list-group-item-heading">Loren Ipsum</h4>
+                            <p class="list-group-item-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam</p>
+                        </a>
+                    </div>
+                </div>
+                    
+            </div>
+        </div>
+        
+        
+        <div class="navbar navbar-default navbar-fixed-bottom">
+            
+            <div class="container">
+                
+                <p class="navbar-text pull-left">Site desenvolvido por Equipe Blog</p>
+                <a href="https://github.com/IFGO/blog" class="navbar-btn btn-danger btn pull-right">Código no GitHub</a>
+                
+            </div>
+            
+        </div>
+        
+        
+        
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+        
+        
+    </body>
+</html>
