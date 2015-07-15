b51e36390ee58ea6b34729bb55d411668f410ccc
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-15 13:54:27 -0300

    menu

--- ProjetoBlog/View/home.php
+++ ProjetoBlog/View/home.php
@@ -10,113 +10,115 @@
     </head>
     <body>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        
-       <div class="navbar navbar-inverse navbar-static-top">
-           <div class="container">
-               
-               <a href="#" class="navbar-brand">Brogão</a>
-               
-               <button class="navbar-toggle" data-toggle ="collapse" data-target =".navHeaderCollapse"
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-               </button>
-               
-               <div class="collapse navbar-collapse navHeaderCollapse">
-                   
-                   <ul class="nav navbar-nav navbar-right">
-                       
-                       <li class="active"><a href="#">Home</a></li>
-                       <li class="dropdown">
-                           
-                           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Redes Sociais<b class="caret"></b></a>
-                           <ul class="dropdown-menu">
-                               <li><a href="#">Twitter</a></li>
-                               <li><a href="#">Facebook</a></li>
-                               <li><a href="#">Instagram</a></li>
-                           </ul>
-                       </li>
-                       <li><a href="#">Sobre o Blog</a></li>
+        <script src="../estilo/js/bootstrap.min.js"></script>
 
-                   
-                   </ul>
-                   
-               </div>
-               
-           </div>
-            
-        </div>
-        
-        
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brog</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="#">Home <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">Sobre</a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Redes Sociais <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Facebook</a></li>
+                                <li><a href="#">Instagram</a></li>
+                                <li><a href="#">Twitter</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
         <div class="container">
-            
+
             <div class="jumbotron">
                 <center><h1>Bem Vindo!</h1>
                     <p>Ao melhor blog de todos os tempos!</p>
                     <a class="btn btn-default">Clique aqui!</a>
                     <a class="btn btn-info">Compartilhe!</a></center>        
             </div>
-            
+
         </div>
-        
-       <div class="container">
-            
+
+        <div class="container">
+
             <div class="row">
-                    
+
                 <div class="col-md-3">
-                    
+
                     <h3><a href="#">Artigo 1 do brog</a></h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
                     <a href="#" class="btn btn-default">Leia Mais</a>
-                    
+
                 </div>
-                
+
                 <div class="col-md-3">
-                    
+
                     <h3><a href="#">Artigo 2 do brog</a></h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
                     <a href="#" class="btn btn-default">Leia Mais</a>
-                    
+
                 </div>
-                
+
                 <div class="col-md-3">
-                    
+
                     <h3><a href="#">Artigo 3 do brog</a></h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
                     <a href="#" class="btn btn-default">Leia Mais</a>
-                    
+
                 </div>
-                
+
                 <div class="col-md-3">
-                    
+
                     <h3><a href="#">Artigo 4 do brog</a></h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel.</p>
                     <a href="#" class="btn btn-default">Leia Mais</a>
-                    
+
                 </div>
-                
+
             </div>
-            
+
         </div>
-        
-        
-        
+
+
+
         <div class="navbar navbar-default navbar-fixed-bottom">
-            
+
             <div class="container">
-                
+
                 <p class="navbar-text pull-left">Site desenvolvido por Equipe Blog</p>
                 <a href="https://github.com/IFGO/blog" class="navbar-btn btn-danger btn pull-right">Código no GitHub</a>
-                
+
             </div>
-            
+
         </div>
-        
-    
-        
-        
-        
+
+
+
+
+
     </body>
 </html>
 
