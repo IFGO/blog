77f9cbd206ea54bb7c3731f1ada0c23933fb871f
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-15 11:13:46 -0300

    Tela login

--- ProjetoBlog/View/Login.php
+++ ProjetoBlog/View/Login.php
@@ -7,47 +7,36 @@ and open the template in the editor.
 <html>
     <head>	
         <title>Brogão› Login</title>
-        <link href="estilo/css/bootstrap.min.css" rel="stylesheet">
-        <link href="estilo/css/styles.css" rel="stylesheet">
-        <link href="../estilo/css/bootstrap.css" rel="stylesheet">
+        <link href="../estilo/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../estilo/css/login.css" rel="stylesheet">
         <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
 
 
     </head>
     <body  style="background: #eee;">
-            <div class="container">
-                   <p><br></p>
-                   <div class="row">
-                          <div class="col-md-8"></div>
-                          <div class="col-md-4">
-                                 <div class="panel panel-default">
-                                        <div class="panel-body">
-                                                  <div class="page-header">
-                                                       <h3>Login</h3>
-                                                  </div>
-                                                  <form>
-                                                            <div class="form-group">
-                                                                      <label for="exampleInputEmail1">Usuário</label>
-                                                                      <div class="input-group">
-                                                                          <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
-                                                                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
-                                                                      </div>
-                                                            </div>
-                                                            <div class="form-group">
-                                                                      <label for="exampleInputPassword1">Senha</label>
-                                                                    <div class="input-group">
-                                                                            <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-star"></span></span>
-                                                                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
-                                                                      </div>
-                                                            </div>
-                                                      <hr/>
-                                                            <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock"></span>Login</button>
-                                                            <p></br></p>
-                                                  </form>
-                                        </div>
-                                 </div>
-                          </div>
-                   </div>
-            </div>   
-    </body>
+        <div class="container">
+            <div class="page-header">
+            </div>
+            <form class="form-login">
+                <h3 class="text-center">Login</h3>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Usuário</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-user"></span></span>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <div class="input-group">
+                        <span class="input-group-addon" id="basic-addon1"><span class="glyphicon glyphicon-star"></span></span>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+                    </div>
+                </div>
+                <hr/>
+                <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock"></span>Login</button>
+                <p></br></p>
+            </form>
+        </div>   
+</body>
 </html>
--- ProjetoBlog/View/post.php
+++ ProjetoBlog/View/post.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="../estilo/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../estilo/css/styles2.css" rel="stylesheet">
+        <link href="../estilo/css/post.css" rel="stylesheet">
     </head>
     <body>
         
