6d0ed6a3369dca723d21ceb3dedd2c8dbd668cb6
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-15 14:18:47 -0300

    includes

--- ProjetoBlog/View/post.php
+++ ProjetoBlog/View/post.php
@@ -9,64 +9,32 @@
         <link href="../estilo/css/post.css" rel="stylesheet">
     </head>
     <body>
-        
-        <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Brog</a>
-                </div>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="#">Home <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">Sobre</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Redes Sociais <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Facebook</a></li>
-                                <li><a href="#">Instagram</a></li>
-                                <li><a href="#">Twitter</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Login</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-        
+        <?php
+        include("header.php");
+        ?>
+
+
         <div class="container">
             <div class="row">
                 <div class="col-lg-9">
                     <div class="panel panel-default"> 
                         <div class="panel-body">
-                            
+
                             <div class="page-header">
                                 <h3>test teste testet tsetstete <small>Postado em 10 de junho</small></h3>
                             </div>
-                            
+
                             <img class="featureImg" src="imagens/android.jpg" width="100%">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In enim lacus, dictum in fringilla vel, sagittis et sem. Nam gravida leo quam, eu ultricies diam commodo id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut pulvinar sapien nec dolor viverra malesuada. Quisque ac justo a mauris vulputate cursus. Sed non mauris neque. Pellentesque tincidunt egestas enim at congue. Nulla congue, metus eu rhoncus vulputate, metus risus suscipit orci, sed congue augue justo sit amet metus.</p>
-                            
+
                             <h4>Texto...</h4>
                         </div>
                     </div>
                 </div>
-                
+
                 <div class="col-lg-3">
                     <div class="list-group">
                         <a href="#" class="list-group-item active">
@@ -87,28 +55,19 @@
                         </a>
                     </div>
                 </div>
-                    
+
             </div>
         </div>
-        
-        
-        <div class="navbar navbar-default navbar-fixed-bottom">
-            
-            <div class="container">
-                
-                <p class="navbar-text pull-left">Site desenvolvido por Equipe Blog</p>
-                <a href="https://github.com/IFGO/blog" class="navbar-btn btn-danger btn pull-right">Código no GitHub</a>
-                
-            </div>
-            
-        </div>
-        
-        
-        
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        
-        
-        
+
+
+        <?php
+        include("footer.php");
+        ?>
+
+
+
+
+
+
     </body>
 </html>
