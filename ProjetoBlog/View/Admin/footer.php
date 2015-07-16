4504fe0159f02dc672118e643b6f446c71d66b8a
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-16 17:49:25 -0300

    footer

---
+++ ProjetoBlog/View/Admin/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Footer</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../estilo/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="../estilo/js/bootstrap.min.js"></script>
+
+        <div class="navbar navbar-default navbar-fixed-bottom">
+
+            <div class="container">
+
+                <p class="navbar-text pull-left">Site desenvolvido por Equipe Blog</p>
+                <a href="https://github.com/IFGO/blog" class="navbar-btn btn-danger btn pull-right">Código no GitHub</a>
+
+            </div>
+
+        </div>
+    </body>
+</html>
+
