ffb9e3a081c6e50e5cfd1a293f33b5010904a30b
{rpa1tera} <{rpa1tera@gmail.com}>
2015-07-16 13:21:25 -0300

    Painel Adm

--- ProjetoBlog/View/Admin/admin.php
+++ ProjetoBlog/View/Admin/admin.php
@@ -10,12 +10,13 @@
     <body>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="../../estilo/js/bootstrap.min.js"></script>
+        <script src="js/padrao.js"></script>
         
         <div class="container-fluid display-table">
             <div class="row display-table-row">
                 <!-- menu lateral -->
                 <div class="col-md-2 col-sm-1 hidden-xs display-table-cell valign-top" id="side-menu">
-                     <h1 class="hidden-xs hidden-sm">Navegação</h1>  
+                     <h1 class="hidden-xs hidden-sm">Painel</h1>  
                      <ul>
                          <li class="link active">
                              <a href="admin.php">
@@ -28,19 +29,23 @@
                              <a href="#collapse-post" data-toggle="collapse" aria-controls="collapse-post">
                                  <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
                                  <span class="hidden-sm hidden-xs">Artigos</span>
-                                 <span class="label label-success pull-right hidden-xs hidden-sm">20</span>
+                                 <span class="label label-success pull-right hidden-xs hidden-sm">10</span>
                              </a>
                              <ul class="collapse collapseable" id="collapse-post">
                                 <li><a href="novo-artigo.php">Adicionar Novo Artigo</a></li>
                                 <li><a href="artigos.php">Listar Artigos</a></li>   
                              </ul>
                          </li>
-
                          <li class="link">
-                             <a href="usuarios.php">
+                             <a href="#collapse-user" data-toggle="collapse" aria-controls="collapse-user">
                                  <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                                  <span class="hidden-sm hidden-xs">Usuários</span>
+                                 <span class="label label-success pull-right hidden-xs hidden-sm">3</span>
                              </a>
+                             <ul class="collapse collapseable" id="collapse-user">
+                                <li><a href="novo-usuario.php">Adicionar Novo Usuário</a></li>
+                                <li><a href="usuarios.php">Listar Usuários</a></li>   
+                             </ul>
                          </li>
                      </ul>
                 </div>
@@ -49,13 +54,13 @@
                     <div class="row">
                         <header id="nav-header" class="clearfix">
                             <div class="col-md-5">
-                            <nav class="navbar-default">
-                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+                            <nav class="navbar-default pull-left">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="offcanvas" data-target="#side-menu" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
                             </nav>
                                 <input type="text" class="hidden-sm hidden-xs"id="header-search-field" placeholder="Pesquisar..."> 
                             </div>
